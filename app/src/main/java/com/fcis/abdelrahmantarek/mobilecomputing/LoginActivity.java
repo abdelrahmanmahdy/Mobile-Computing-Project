c3041c3c49da9fc54e39b27862ff1eb103718f7b
AbdelRahman Tarek <abdelrahman.tare.mahdy@gmail.com>
2017-12-10 18:30:45 +0200

    login finished

--- app/src/main/java/com/fcis/abdelrahmantarek/mobilecomputing/LoginActivity.java
+++ app/src/main/java/com/fcis/abdelrahmantarek/mobilecomputing/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.fcis.abdelrahmantarek.mobilecomputing;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
@@ -16,11 +19,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText email,pw;
-    CheckBox remeber;
-    Button login,register;
+    EditText email, pw;
+    CheckBox remember;
+    Button login, register;
     TextView forgot;
     private boolean emailGood;
     private boolean pwGood;
@@ -32,10 +35,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Login");
         setSupportActionBar(toolbar);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean loggedin = preferences.getBoolean("remember", false);
+        if(loggedin){
+            Intent intent = new Intent(this,HomeActivity.class);
+            startActivity(intent);
+        }
         //init UI
         email = findViewById(R.id.email_ET);
         pw = findViewById(R.id.pw_ET);
-        remeber = findViewById(R.id.remember_CB);
+        remember = findViewById(R.id.remember_CB);
         login = findViewById(R.id.login_BTN);
         register = findViewById(R.id.register_BTN);
         forgot = findViewById(R.id.fp_TV);
@@ -53,11 +62,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String emailIn = email.getText().toString();
-                if(emailIn.isEmpty()){
-                    emailGood=false;
+                if (emailIn.isEmpty()) {
+                    emailGood = false;
                     email.setError(null);
-                }else
-                if (!Patterns.EMAIL_ADDRESS.matcher(emailIn).matches()) {
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(emailIn).matches()) {
                     email.setError("Wrong email format!");
                     emailGood = false;
                 } else {
@@ -80,11 +88,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String password = pw.getText().toString();
-                if(password.isEmpty()){
-                    pwGood=false;
+                if (password.isEmpty()) {
+                    pwGood = false;
                     pw.setError(null);
-                }else
-                if (password.length() < 8) {
+                } else if (password.length() < 8) {
                     pw.setError("Password too short!");
                     pwGood = false;
                 } else {
@@ -104,15 +111,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.register_BTN:
-                Toast.makeText(this,"Register",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
                 break;
             case R.id.login_BTN:
-                Toast.makeText(this,"Login",Toast.LENGTH_LONG).show();
+                if(emailGood && pwGood) {
+                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("remember", remember.isChecked());
+                    editor.apply();
+                    Intent inten = new Intent(this, HomeActivity.class);
+                    startActivity(inten);
+                    finish();
+                }
                 break;
             case R.id.fp_TV:
-                Toast.makeText(this,"Forgot password",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Forgot password", Toast.LENGTH_LONG).show();
                 break;
 
         }
